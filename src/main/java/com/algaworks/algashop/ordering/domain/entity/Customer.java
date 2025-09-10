cc793aa2b40ad44064a45035e42c52318b8980c1
spark <fabricio.jacob@outlook.com>
2025-09-09 22:49:46 -0300

    init project.

--- src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
@@ -1,16 +1,15 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
 import com.algaworks.algashop.ordering.domain.exception.CustomerArchivedException;
-import com.algaworks.algashop.ordering.domain.validator.FieldValidations;
 import com.algaworks.algashop.ordering.domain.valueobject.*;
+import com.algaworks.algashop.ordering.domain.valueobject.id.CustomerId;
 import lombok.Builder;
 
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
-import static com.algaworks.algashop.ordering.domain.exception.ErrorMessages.*;
+import static com.algaworks.algashop.ordering.domain.exception.ErrorMessages.VALIDATION_ERROR_FULLNAME_IS_NULL;
 
 public class Customer {
     private CustomerId id;
