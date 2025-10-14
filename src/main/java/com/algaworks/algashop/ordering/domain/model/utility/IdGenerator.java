e012fcf4b1f133af6c498e323f1b3edd42fce21a
fabricio <fabricio.jacob@outlook.com>
2025-10-13 21:54:50 -0300

    add project.

--- src/main/java/com/algaworks/algashop/ordering/domain/model/utility/IdGenerator.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/utility/IdGenerator.java
@@ -24,8 +24,16 @@ public class IdGenerator {
      * TSID_NODE
      * TSID_NODE_COUNT
      */
-    public static TSID gererateTSID() {
+    public static TSID generateTSID() {
         return tsidFactory.generate();
+    }
+
+    public static TSID generateTSID(String value) {
+        return TSID.from(value);
+    }
+
+    public static TSID generateTSID(Long value) {
+        return TSID.from(value);
     }
 
 }
